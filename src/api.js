3e578841b0fb9e5b2f619fd845cb4ad03bc4b9ab
miurakenntaro <114901350+miurakenntaro@users.noreply.github.com>
2022-12-19 15:36:53 +0900

    Delete src directory

--- src/api.js
+++
@@ -1,7 +0,0 @@
-export async function fetchImages(breed) {
-    const response = await fetch(
-      `https://www.amiiboapi.com/api/amiibo/?name=${breed}`
-    );
-    const data = await response.json();
-    return data.amiibo;
-  }
